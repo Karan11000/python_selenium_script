f7499031f9d576687fd283c7110b42c94e19c0f5
Karan11000 <76610086+Karan11000@users.noreply.github.com>
2022-11-15 18:59:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
-import os
 driver = webdriver.Chrome()
 driver.maximize_window()
-driver.get("https://www.netflix.com/in/login")
-driver.find_element("name", "userLoginId").send_keys(os.environ['email'])
-driver.find_element("name", "password").send_keys(os.environ['password'])
-driver.find_element("xpath", "/html/body/div[1]/div/div[3]/div/div/div[1]/form/button").send_keys(Keys.ENTER)
+driver.get("https://www.netflix.com/in/browse/genre/839338")
+driver.find_element("xpath", "/html/body/div[1]/div/div[2]/main/section[2]/div/ul/li[2]/a").send_keys(Keys.ENTER)
 time.sleep(4)
-driver.find_element("link_text", "General").click()
-driver.find_element("link_text", "Suits").click()
-time.sleep(4)
+title = driver.find_element("xpath", "/html/body/div[1]/div/div[2]/section[1]/div[1]/div[1]/div[2]/div/h1")
+description = driver.find_element("xpath", "/html/body/div[1]/div/div[2]/section[1]/div[1]/div[1]/div[2]/div/div[2]/div[1]")
+print(title.text)
+print(description.text)
+element = driver.find_element("xpath", "/html/body/div[1]/div/div[2]/section[3]/div[2]/ul/li[1]/div/button")
+driver.execute_script("return arguments[0].scrollIntoView();", element)
+time.sleep(2)
+element.send_keys(Keys.ENTER)
+time.sleep(100)
